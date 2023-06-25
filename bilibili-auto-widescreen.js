1f3c4a4b23e3f680e2c6a3c1fc595240ff3adb7e
Won Li <42350954+wonli17@users.noreply.github.com>
2023-06-26 00:45:11 +0800

    Update bilibili-auto-widescreen.js

--- bilibili-auto-widescreen.js
+++ bilibili-auto-widescreen.js
@@ -16,14 +16,55 @@
         return new Promise(resolve => setTimeout(resolve, ms))
     }
 
+    const remove = (p_node) => {
+        if(p_node.tagName === 'LI'){
+            p_node.remove();
+            return;
+        }
+        remove(p_node.parentNode);
+        console.log("#remove");
+    }
+
+    const removeBlacklist = () => {
+        var blocklist = [
+            'right-entry-item--upload',
+            'right-entry-item',
+            'right-entry--message',
+            'follow-btn',
+            'right-entry--vip',
+            'adblock-tips',
+            'recommended-swipe',
+            'channel-items__right'
+        ];
+
+        blocklist.forEach((d, i) => {
+            const dom = document.getElementsByClassName(d);
+            if(dom.length !== 0){
+                if(dom.length !== 1) {
+                    dom.forEach((dd, ii) => {
+                        dd.remove();
+                    });
+                }else {
+                    dom[0].remove();
+                }
+            }
+        });
+
+        document.getElementsByClassName('right-entry-text').forEach((d,i) => {
+            if(['动态', '收藏', '历史'].indexOf(d.innerHTML) === -1){
+                remove(d.parentNode);
+            }
+        });
+    }
+
     async function ctrlWideLoop() {
         const ctrlWideBtn = document.getElementsByClassName('bpx-player-ctrl-wide')[0];
         if(!ctrlWideBtn){
             await sleep(200);
             ctrlWideLoop();
-        }
-        if([...ctrlWideBtn.classList].indexOf('bpx-state-entered') === -1) {
+        }else if([...ctrlWideBtn.classList].indexOf('bpx-state-entered') === -1) {
             ctrlWideBtn.click();
+            removeBlacklist();
             return;
         }
     }
